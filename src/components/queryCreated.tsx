b2e1a4e376c2085486937636c50d69739ad5b929
moxy <louise.moxhay@blackswan.com>
2020-08-10 06:39:41 +0100

    Query created view

---
+++ src/components/queryCreated.tsx
@@ -0,0 +1,45 @@
+import * as React from 'react'
+import { dataSource } from '../dataSource/dataSource'
+import { SelectedIdContext } from '../context/selectedIdContext'
+import { Container } from './GlobalStyles'
+import styled from 'styled-components'
+import { Card, CardContent, Typography } from '@material-ui/core'
+import { InfoCard } from './infoCard'
+import { Phone } from './phone'
+
+export const QueryCreated = () => {
+  const { selectedId } = React.useContext(SelectedIdContext)
+  const searchQuery = dataSource.getSearchQuery(selectedId)
+  return (
+    <Container>
+      <Columns>
+        <InfoCard text={infoText} />
+        <div>
+          <Typography variant="body1" style={{ padding: '20px 0' }}>
+            The following search query has been generated:
+          </Typography>
+          <Card>
+            <CardContent>
+              <p>{searchQuery}</p>
+            </CardContent>
+          </Card>
+        </div>
+      </Columns>
+      <Phone/>
+    </Container>
+  )
+}
+
+const infoText =
+  'Info panel explaining what the tech is doing in the back end. E.g. using John Snow Labs entity recognition model, topic expansion, referencing synonyms'
+
+const Columns = styled.div`
+  display: flex;
+  align-items: center;
+  & > *:first-child {
+    flex: 1;
+  }
+  & > *:nth-child(2) {
+    flex: 3;
+  }
+`
